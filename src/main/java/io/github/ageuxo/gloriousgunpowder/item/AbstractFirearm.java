2446163bf699c101a67dd266e1a1db688278f5d0
TheDrOfDoctoring <59176662+TheDrOfDoctoring@users.noreply.github.com>
2024-05-15 17:02:57 +0400

    Create Abstract Firearm (#2)
    * abstract guns
    
    * consistent floating

---
+++ src/main/java/io/github/ageuxo/gloriousgunpowder/item/AbstractFirearm.java
@@ -0,0 +1,27 @@
+package io.github.ageuxo.gloriousgunpowder.item;
+
+import io.github.ageuxo.gloriousgunpowder.data.GunAttribute;
+import io.github.ageuxo.gloriousgunpowder.data.GunComponents;
+import io.github.ageuxo.gloriousgunpowder.data.GunDataComponents;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractFirearm extends Item {
+    public AbstractFirearm(Properties pProperties) {
+        super(pProperties);
+    }
+    public void setGunAttributes(ItemStack stack, GunAttribute gunAttribute) {
+        stack.set(GunDataComponents.GUN_ATTRIBUTES, gunAttribute);
+    }
+    public GunAttribute getGunAttributes(ItemStack stack) {
+        return stack.getOrDefault(GunDataComponents.GUN_ATTRIBUTES, new GunAttribute(0f, 1f, 1f));
+    }
+    public void setNewComponents(ItemStack stack, List<GunComponents> components) {
+        List<GunComponents> existingComponentData = new ArrayList<>(stack.getOrDefault(GunDataComponents.GUN_COMPONENTS, new ArrayList<>()));
+        existingComponentData.addAll(components);
+        stack.set(GunDataComponents.GUN_COMPONENTS, existingComponentData);
+    }
+}
