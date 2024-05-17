72ff59243b134764687cc2bbf59c951319aa2d11
ageuxo <ageuxo@gmail.com>
2024-05-17 15:23:34 +0200

    Added GunStats.java to hold GunStat constants Added DAMAGE, RELOAD_TIME, and ACCURACY GunStat constants

---
+++ src/main/java/io/github/ageuxo/gloriousgunpowder/data/stats/GunStats.java
@@ -0,0 +1,9 @@
+package io.github.ageuxo.gloriousgunpowder.data.stats;
+
+public class GunStats {
+    public static final GunStat DAMAGE = new GunStat("damage", 2);
+    public static final GunStat RELOAD_TIME = new GunStat("reload_time", 4);
+    public static final GunStat ACCURACY = new GunStat("accuracy", 1);
+
+
+}
