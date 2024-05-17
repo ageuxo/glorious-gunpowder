fc40c48d8d4e1b4af36a8287bc54de8032758a8e
ageuxo <ageuxo@gmail.com>
2024-05-17 16:57:38 +0200

    Add example datagen for MaterialProvider Add datagenned material

--- src/main/java/io/github/ageuxo/gloriousgunpowder/datagen/MaterialProvider.java
+++ src/main/java/io/github/ageuxo/gloriousgunpowder/datagen/MaterialProvider.java
@@ -2,13 +2,18 @@ package io.github.ageuxo.gloriousgunpowder.datagen;
 
 import io.github.ageuxo.gloriousgunpowder.GloriousGunpowderMod;
 import io.github.ageuxo.gloriousgunpowder.data.Material;
+import io.github.ageuxo.gloriousgunpowder.data.PartShape;
+import io.github.ageuxo.gloriousgunpowder.data.stats.GunStatModifier;
+import io.github.ageuxo.gloriousgunpowder.data.stats.GunStats;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -19,12 +24,28 @@ public class MaterialProvider extends DatapackBuiltinEntriesProvider {
         super(output, registries, BUILDER, Set.of(GloriousGunpowderMod.MOD_ID));
     }
 
-    public static void bootstrap(BootstrapContext<Material> context){
-
-    }
-
     @Override
     public @NotNull String getName() {
         return "Material";
+    }
+
+    public static final ResourceKey<Material> IRON = createKey("iron");
+    public static final ResourceKey<Material> WOOD = createKey("wood");
+
+    public static void bootstrap(BootstrapContext<Material> context){
+        context.register(IRON, new Material("iron",
+                List.of(
+                        new GunStatModifier(GunStats.RELOAD_TIME.get(), GunStatModifier.Type.ADDITION, "iron_reload", 2),
+                        new GunStatModifier(GunStats.DAMAGE.get(), GunStatModifier.Type.MULTIPLIER, "iron_damage", 1.5)
+                )));
+        context.register(WOOD, new Material("wood",
+                List.of(
+                        new GunStatModifier(GunStats.RELOAD_TIME.get(), GunStatModifier.Type.MULTIPLIER, "wood_reload", 0.8),
+                        new GunStatModifier(GunStats.DAMAGE.get(), GunStatModifier.Type.MULTIPLIER, "wood_damage", 0.8)
+                )));
+    }
+
+    private static ResourceKey<Material> createKey(String name){
+        return ResourceKey.create(Material.KEY, GloriousGunpowderMod.rl(name));
     }
 }
