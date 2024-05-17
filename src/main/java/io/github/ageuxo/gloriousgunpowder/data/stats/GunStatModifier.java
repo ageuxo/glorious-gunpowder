6bae6fddfec04694a13117eb3f50e787d291eb88
ageuxo <ageuxo@gmail.com>
2024-05-17 21:32:19 +0200

    Change GunStatModifier CODEC to use String instead of int for Type

--- src/main/java/io/github/ageuxo/gloriousgunpowder/data/stats/GunStatModifier.java
+++ src/main/java/io/github/ageuxo/gloriousgunpowder/data/stats/GunStatModifier.java
@@ -3,34 +3,27 @@ package io.github.ageuxo.gloriousgunpowder.data.stats;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.ageuxo.gloriousgunpowder.GunRegistries;
-import net.minecraft.core.Holder;
 
 public record GunStatModifier(GunStat gunStat, Type type, String id, double value) {
 
     public static final Codec<GunStatModifier> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             GunRegistries.GUN_STATS.byNameCodec().fieldOf("stat").forGetter(GunStatModifier::gunStat),
-            Codec.INT.fieldOf("type").forGetter(GunStatModifier::typeId),
+            Codec.STRING.fieldOf("type").forGetter(GunStatModifier::typeId),
             Codec.STRING.fieldOf("id").forGetter(GunStatModifier::id),
             Codec.DOUBLE.fieldOf("value").forGetter(GunStatModifier::value)
     ).apply(instance, GunStatModifier::new));
 
-    public GunStatModifier(GunStat gunStat, int typeOrdinal, String id, double value){
-        this(gunStat, Type.fromOrdinal(typeOrdinal), id, value);
+    public GunStatModifier(GunStat gunStat, String typeId, String id, double value){
+        this(gunStat, Type.valueOf(typeId), id, value);
     }
 
-    public int typeId(){
-        return type.ordinal();
+    public String typeId(){
+        return type.name();
     }
 
     public enum Type{
         ADDITION,
-        MULTIPLIER;
-
-        private static final Type[] VALUES = Type.values();
-
-        public static Type fromOrdinal(int ordinal){
-            return VALUES[ordinal];
-        }
+        MULTIPLIER
 
     }
 }
